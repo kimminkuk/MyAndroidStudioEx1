923e530bf48e4dae565f6ca0c38902eff8045aa7
mikuk <rlaal567@gmail.com>
2021-07-07 22:22:01 +0900

    CallIntent

---
+++ main/java/com/techcom/mycallintent/MainActivity.java
@@ -0,0 +1,30 @@
+package com.techcom.mycallintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.editTextTextPersonName);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String receiver = editText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + receiver));
+                startActivity(intent); //시스템쪽으로 전달한다.
+            }
+        });
+    }
+}
